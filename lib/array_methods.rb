2acb7671f1bd5dd4f3652045e48bd81351e88f78
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-29 17:22:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,12 +4,12 @@ def find_element_index
  end
 
 def find_max_value
-  find_max_value([1,2,1,3,4,3,5,4,3,2,1])
+
 
   # Add your solution here
 end
 
 def find_min_value
-  find_min_value([10,5,3,7,19,1,3,10])
+
   # Add your solution here
 end
